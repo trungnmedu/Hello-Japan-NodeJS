adba58564ff9cd102e9cc05552f0c0fc32c3d522
Minh Trung <nguyenminhtrungse@gmail.com>
2023-05-30 09:40:57 +0700

    Update expire socket

--- src/services/chat.service.js
+++ src/services/chat.service.js
@@ -2,7 +2,9 @@ const RedisDatabase = require('@databases/redis.database')
 
 class ChatService {
     static async saveClient(userId, socketId) {
-        await (await RedisDatabase.getInstance()).lPush(userId, socketId)
+        const redisInstance = await RedisDatabase.getInstance()
+        await redisInstance.lPush(userId, socketId)
+        await redisInstance.expire(userId, 24 * 60 * 60)
     }
 
     static async removeClient(userId, socketId) {
